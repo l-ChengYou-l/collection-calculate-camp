09b91f36aecf8d112b255f7f10aba55fe69621be
l-ChengYou-l <l-chenchen-l@hotmail.com>
2018-04-02 21:17:53 +0800

    修改

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,15 +1,18 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-  var no_repeat=[]
+  /*var no_repeat=[]
   for(var i= 0;i<collection.length;i++){
-    for(var j= 0;i<collection.length;j++){
+    for(var j= 0;j<collection.length;j++){
       if(collection[i] !== collection[j]) {
-        no_repeat.push(collection[i])
+        no_repeat.push(collection[j])
       }
     }
   }
-  return no_repeat
+  return no_repeat*/
+
+  var no_repeat = new Set(collection)
+  return [...no_repeat]
 }
 
 module.exports = choose_no_repeat_number;
